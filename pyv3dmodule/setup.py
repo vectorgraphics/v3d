3c9c0e1ae80c666ac53a5fb754f83d8d26ad9386
John Bowman <bowman@ualberta.ca>
2021-08-13 23:43:41 -0600

    Update URL.

--- pyv3dmodule/setup.py
+++ pyv3dmodule/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(name='pyv3d',
       version='0',
       description='Python library for reading V3D Files',
-      url='asymptote.sourceforge.io',
+      url='https://github.com/vectorgraphics/v3d',
       author='Supakorn "Jamie" Rassamememasmuang and John C. Bowman',
       author_email="jamievlin@outlook.com, bowman@ualberta.ca",
       license='Apache 2.0',
